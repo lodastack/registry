79b0faab3843e377462463b57e5ef3e8ea4c2d1f
ianwoolf <btw515wolf2@gmail.com>
2016-12-02 15:11:47 +0800

    update Access-Control-Allow-Headers

--- model/template.go
+++ model/template.go
@@ -175,22 +175,26 @@ var (
 		"name":             "time.offset",
 		"interval":         "300",
 		"measurement_type": "TIME",
-	}, Resource{
+	}, /* Resource{
 		"connect_timeout":  "3",
 		"interval":         "10",
 		"measurement_type": "PORT",
 		"name":             "port.sshd.22",
 		"port":             "22",
-	},
-	/*
-	   {
-	     "git": "git@git.xxx.com:plugins/process.git",
-	     "interval": "10",
-	     "measurement_type": "PLUGIN",
-	     "name": "xxx",
-	     "parameters": "-x xxx"
-	   }
-	*/}
+	}, Resource{
+		"git":              "git@git.loda.com:plugins/process.git",
+		"interval":         "10",
+		"measurement_type": "PLUGIN",
+		"name":             "loda-plugin",
+		"parameters":       "-x loda",
+	}, Resource{
+		"bin_path":         "/usr/local/registry/bin/registry",
+		"comment":          "registry",
+		"interval":         "10",
+		"measurement_type": "PROC",
+		"name":             "bin.registry",
+	},*/
+	}
 	RootTemplate map[string]Resources = map[string]Resources{
 		TemplatePrefix + "doc":     docTemplate,
 		TemplatePrefix + "collect": collectTemplate,
