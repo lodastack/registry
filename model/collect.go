9a2f7a6af58c33207f9e8e6bcc90aac737e94289
kun <oiooj@qq.com>
2018-06-19 16:45:03 +0800

    fix replace bug

--- model/collect.go
+++ model/collect.go
@@ -100,7 +100,7 @@ func UpdateCollectName(collects ...Resource) error {
 		// proc monitor: /sbin/python2.7, the collect name will be pythone2.7
 		// we replace "." with "-".
 		if collectType == ProcCollect {
-			collects[index]["name"] = strings.Replace(collects[index]["name"], ".", "-", 0)
+			collects[index]["name"] = strings.Replace(collects[index]["name"], ".", "-", -1)
 		}
 
 		for _, nameLetter := range collects[index]["name"] {
