7057e564aee67db66c0eb1f22c0f104ce80b4aa8
kun <oiooj@qq.com>
2018-07-02 14:58:43 +0800

    deploy name should be a DNS 1123 label

---
+++ httpd/validation.go
@@ -0,0 +1,21 @@
+package httpd
+
+import "regexp"
+
+const dns1123LabelFmt string = "[a-z0-9]([-a-z0-9]*[a-z0-9])?"
+const dns1123LabelErrMsg string = "a DNS-1123 label must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character"
+const dns1123LabelMaxLength int = 63
+
+var dns1123LabelRegexp = regexp.MustCompile("^" + dns1123LabelFmt + "$")
+
+// IsDNS1123Label tests for a string that conforms to the definition of a label in
+// DNS (RFC 1123).
+func IsDNS1123Label(value string) bool {
+	if len(value) > dns1123LabelMaxLength {
+		return false
+	}
+	if !dns1123LabelRegexp.MatchString(value) {
+		return false
+	}
+	return true
+}
